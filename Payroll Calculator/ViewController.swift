c4756775d99143237c3d6ded09da8de9881b3ed2
student19 <student19@student20s-macbook-air.local>
2019-02-07 20:31:55 -0600

    Initial Commit

---
+++ Payroll Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Payroll Calculator
+//
+//  Created by student19 on 2/7/19.
+//  Copyright © 2019 Jose Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
